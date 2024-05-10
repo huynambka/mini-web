615a8db789ee5349c147be8f84ba46524b5c05c1
Nguyen Huy Nam <huynambka@gmail.com>
2024-05-10 08:52:14 +0700

    init server and database

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const start = async () => {
+    try {
+        // Connect DB
+        const { url } = require('./config/db.config');
+        const mongoose = require('mongoose');
+        mongoose
+            .connect(url)
+            .then(() => {
+                console.log('Connected to the database!');
+            })
+            .catch((err) => {
+                console.log('Cannot connect to the database!', err);
+                process.exit();
+            });
+        app.listen(process.env.PORT, () => {
+            console.log(
+                `Server is running on port ${process.env.PORT || 8080}`
+            );
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
