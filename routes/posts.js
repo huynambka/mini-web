9367daeaee28db0fc2149899a6749013c1d4c94a
Nguyen Huy Nam <huynambka@gmail.com>
2024-05-11 14:59:03 +0700

    fix get post route

--- routes/posts.js
+++ routes/posts.js
@@ -5,7 +5,7 @@ const passportJWT = passport.authenticate('jwt', { session: false }, null);
 const { createPost, getPost, getPublicPost } = require('../controllers/posts');
 
 router.post('/create', passportJWT, createPost);
-router.get('/:id', getPost);
+router.get('/:id', passportJWT, getPost);
 router.get('/', passportJWT, getPublicPost);
 
 module.exports = router;
